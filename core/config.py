d18901dcb43c8d0ce3f25b46d0a66a1237a9e5df
husnasherin327-oss <husnasherin327@gmail.com>
2026-07-31 11:07:53 +0530

    gemii model change

--- core/config.py
+++ core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     APP_PORT: int = 8000
 
     GEMINI_API_KEY: str = "YOUR_GEMINI_API_KEY_HERE"
-    GEMINI_MODEL: str = "gemini-2.5-flash-lite"
+    GEMINI_MODEL: str = "gemini-flash-latest"
 
     ALLOWED_ORIGINS: List[str] = ["*"]
 
